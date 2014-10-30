8ac1ca8bb7486693f5aedcf935fda82d0fce3665
heeger <heeger@mi.fu-berlin.de>
2014-10-30 15:25:59 +0100

    add the NCBI SOAP Toolbox

---
+++ NcbiTools/NcbiSoap.py
@@ -0,0 +1,318 @@
+import csv
+import sys
+import time
+
+from suds.client import Client as SoapClient
+from suds import plugin
+
+class NcbiSoapMap(dict):
+    """Base class for a dictonary relying in NCBI soap interface for assignments.
+    
+    This is an abstract base class for a dictonary that uses the NCBI soap 
+    interface to represent relations between certain NCBI governed objects like
+    genes, taxonomic objects or genomes.
+    Key requests will be looked up from a dictonary. If the key is not available
+    a request to NCBI will be send via the SOAP interface. The result of this 
+    lookup will be stored in the local dictonary. The detaileds of the request
+    have to be implemeneted by inheriting calsses.
+    The local dictonary can be saved to the hard drive as csv file and loaded
+    when an object is copnstructed.
+    """
+    def __init__(self, indict={}, cachePath=None, retry=0):
+        """Constuctor for mapping object.
+        
+        A dictonary of already known assignments can be passed via the indict
+        parameter. The cachePath can is the path were the save and load fucntion
+        will search for a csv file. retry gives the number of times a request 
+        should be send to NCBI after again before giving up (0 means only send 
+        once).
+        """
+        dict.__init__(self, indict)
+        if not cachePath:
+            #if no cache path was given default to the class name
+            cachePath = self.__class__.__name__+".csv"
+        self.cachePath = cachePath
+        self.retry = retry
+        try:
+            self.load()
+        except IOError:
+            pass
+            #this happens if no cache was saved previously
+        
+    def __getitem__(self, key):
+        if key not in self:
+            for tries in range(self.retry+1):
+                try:
+                    response = self.requestFunction(key)
+                    break
+                except Exception as e:
+                    if tries == self.retry:
+                        raise KeyError("Problem with key: '%s'. "
+                                       "It raised an error: %s" 
+                                       % (key, str(e)))
+                    else:
+                        time.sleep(1)
+                        sys.stderr.write("Problem with key: '%s'. "
+                                         "It raised an error: %s\n "
+                                         "Will try again...\n" % (key, str(e)) )
+            self.readResponse(response, key)
+        return dict.__getitem__(self, key)
+            
+    def requestFunction(key):
+        raise NotImplemented("This base class does not implement any request. "
+                             "Use a more specific subclass.")
+            
+    def load(self):
+        for row in csv.reader(open(self.cachePath, "rb")):
+            self[row[0]] = row[1]
+    
+    def save(self):
+        csv.writer(open(self.cachePath, "wb")).writerows(zip(self.keys(), self.values()))
+
+class NcbiSoapBatchMap(NcbiSoapMap):
+    """Mapping object that will take a list of key and return a list of values 
+    instead of a single key value pair. Requests to NCBI will be done in batch 
+    mode with all keys that are not in the cache."""
+    
+    def __getitem__(self, keyList):
+        for key in keyList:
+            ncbiReqList = [key for key in keyList if key not in self]
+            
+            for tries in range(self.retry+1):
+                try:
+                    response = self.requestFunction(",".join([str(k) for k in ncbiReqList]))
+                    break
+                except Exception as e:
+                    if tries == self.retry:
+                        raise ValueError("Problem with key: '%s'. It raised an error: %s" % (key, str(e)))
+                    else:
+                        time.sleep(1)
+                        sys.stderr.write("Problem with key: '%s'. It raised an error: %s\n Will try again...\n" % (key, str(e)) )
+            self.readResponse(response, keyList)
+        return [dict.__getitem__(self, key) for key in keyList]
+
+class SpeciesName2TaxId(NcbiSoapMap):
+    """Map scientific names of species to their NCBI taxonomy DB ID.
+    
+    """
+    wsdlUrl="http://www.ncbi.nlm.nih.gov/soap/v2.0/eutils.wsdl"
+    
+    def requestFunction(self, key):
+        cl = SoapClient(self.wsdlUrl)
+        return cl.service.run_eSearch("taxonomy", key)
+        
+    def readResponse(self, resp, key):
+        if int(resp.Count) > 1:
+            self[key] = None
+            raise ValueError("Problem with key: %s. It got multiple answers.")
+        self[key] = resp.IdList[0][0]
+
+class LineageMap(NcbiSoapMap):
+    wsdlUrl = "http://www.ncbi.nlm.nih.gov/soap/v2.0/efetch_taxon.wsdl"
+    
+    def requestFunction(self, key):
+        cl = SoapClient(self.wsdlUrl)
+        return cl.service.run_eFetch(str(key))
+    
+    def readResponse(self, resp, key):
+        m = []
+        for r in resp.TaxaSet[0][0]["LineageEx"][0]:
+            m.append((r["Rank"], r["TaxId"], r["ScientificName"]))
+        self[key] = m
+        
+    def save(self):
+        tab = []
+        for tax, m in self.items():
+            for rank, (lTax, lName) in m.items():
+                tab.append([tax, rank, lTax, lName])
+        csv.writer(open(self.cachePath, "wb")).writerows(tab)
+    
+    def load(self):
+        for row in csv.reader(open(self.cachePath, "rb")):
+            tax, rank, lTax, lName = row
+            if tax not in self:
+                self[tax] = {}
+            self[tax][rank] = (lTax, lName)
+
+
+class NuclId2TaxIdMap(NcbiSoapMap):
+    
+    wsdlUrl = "http://www.ncbi.nlm.nih.gov/soap/v2.0/efetch_seq.wsdl"
+    
+    def requestFunction(self, key):
+        cl = SoapClient(self.wsdlUrl)
+        return cl.service.run_eFetch("nucleotide", str(key))
+    
+    def readResponse(self, resp, key):
+        if len(resp) < 1:
+            raise KeyError("'%s' is not in the dictionary. NCBI response did not contain taxonomy information.")
+        if len(resp) > 1:
+            self[key] = None
+            raise ValueError("Problem with key: %s. It got multiple answers." % key)
+        
+        #if there is only one feature for some reason the feature list is not actually a list
+        #here is a work around:
+        if type(resp["GBSet"][0]["GBSeq_feature-table"][0]) != type([]):
+            feature_list = [resp["GBSet"][0]["GBSeq_feature-table"][0]]
+        else:
+            feature_list = resp["GBSet"][0]["GBSeq_feature-table"][0]
+        for feature in feature_list:
+            if feature["GBFeature_key"] == "source":
+                for qual in feature["GBFeature_quals"][0]:
+                    if qual["GBQualifier_name"] == "db_xref":
+                        match = re.match("taxon:(\d+)", qual["GBQualifier_value"])
+                        if match:
+                            self[key] = match.group(1)
+                            return True
+        raise KeyError("'%s' is not in the dictonary. NCBI response did not contain taxonomy inforamtion. NCBI response was:\n%s" % (key, str(resp)[:100]))
+
+class NuclId2SpeciesNameMap(NcbiSoapMap):
+    
+    wsdlUrl = "http://www.ncbi.nlm.nih.gov/soap/v2.0/efetch_seq.wsdl"
+    
+    def requestFunction(self, key):
+        cl = SoapClient(self.wsdlUrl)
+        return cl.service.run_eFetch("nucleotide", str(key))
+    
+    def readResponse(self, resp, key):
+        if len(resp) < 1:
+            raise KeyError("'%s' is not in the dictionary. NCBI response did not contain taxonomy information.")
+        if len(resp) > 1:
+            self[key] = None
+            import pdb; pdb.set_trace()
+            raise ValueError("Problem with key: %s. It got multiple answers." % key)
+        organism = resp[0][0].GBSeq_organism
+        if "Unknown" in organism:
+            raise ValueError("Source of sequence is given as 'unknown' in NCBI")
+        self[key] = organism
+        
+
+#class NuclId2TaxIdBatchMap(NuclId2SpeciesNameMap):
+#    def readResponse(self, resp, keyList):
+#        if len(resp) < 1:
+#            raise KeyError("'%s' is not in the dictionary. NCBI response did not contain taxonomy information.")
+#        gbset = r["GBSet"]
+#        if len(gbset[0]) == 1:
+#            gbset = 
+
+
+#class ProtI
+class DebugPlugin(plugin.MessagePlugin):
+    def received(self, context):
+        import pdb; pdb.set_trace()
+
+
+class InitDebugPlugin(plugin.InitPlugin):
+    def initialized(self, context):
+        import pdb; pdb.set_trace()
+
+
+if __name__ == "__main__":
+    import logging
+    import re
+
+    def lineageTest(a,b):
+        for rowA, rowB in zip(a, b):
+            for colA, colB in zip(rowA, rowB):
+                if colA != colB:
+                    return False
+        return True
+
+#    logging.basicConfig(level=logging.INFO)
+#    logging.getLogger('suds.client').setLevel(logging.DEBUG)
+    
+    sciNam2IdTestSet = {"Homo sapiens": "9606",
+                        "Mus musculus": "10090",
+                        "Human immunodeficiency virus 1": "11676",
+                        "Escherichia coli": "562",
+                        "Clavariopsis aquatica": "253306",
+                        }
+                        
+    nucl2taxNameTestSet = {297822720: "Arabidopsis lyrata subsp. lyrata",
+                           237825467: "Clavariopsis aquatica",
+                           21694053: "Homo sapiens",
+                           34559759: "Rupicapra rupicapra",
+                           666359714: "Ursus maritimus",
+                           }
+                       
+    nucl2taxIdTestSet = {297822720: "81972",
+                         237825467: "253306",
+                         21694053: "9606",
+                         34559759: "34869",
+                         666359714: "29073",
+                         }
+                         
+    
+    print "Running Ncbi Soap Tool tests"
+    
+    print "testing scientific name to taxonomy ID map:"
+    sciName2taxId = SpeciesName2TaxId()
+    print "Successfully build mapping object"
+    for key, value in sciNam2IdTestSet.items():
+        sys.stdout.write("%s\t" % key)
+        tId = str(sciName2taxId[key])
+        sys.stdout.write(tId)
+        if tId == value:
+            sys.stdout.write("\tOK\n")
+        else:
+            sys.stdout.write("\tFailed. Unexpected value: %s\n" % tId)
+        time.sleep(1)
+    
+    print "testing nuclear ID to scientific name (taxonomy) map:"
+    nuclId2taxName = NuclId2SpeciesNameMap()
+    print "Successfully build mapping object"
+    for key, value in nucl2taxNameTestSet.items():
+        sys.stdout.write("%s\t" % key)
+        tId = str(nuclId2taxName[key])
+        sys.stdout.write(tId)
+        if tId == value:
+            sys.stdout.write("\tOK\n")
+        else:
+            sys.stdout.write("\tFailed. Unexpected value: %s\n" % tId)
+        time.sleep(1)
+    
+    print "testing nuclear ID to taxonomy ID map:"
+    nuclId2taxId = NuclId2TaxIdMap()
+    print "Successfully build mapping object"
+    for key, value in nucl2taxIdTestSet.items():
+        sys.stdout.write("%s\t" % key)
+        try:
+            tId = str(nuclId2taxId[key])
+        except Exception as e:
+            import traceback
+            sys.stderr.write(traceback.format_exc())
+            sys.stdout.write("\tFailed. Exception raised: \"%s\". See log file for more information\n" % str(e))
+            continue
+        sys.stdout.write(tId)
+        if tId == value:
+            sys.stdout.write("\tOK\n")
+        else:
+            sys.stdout.write("\tFailed. Unexpected value: %s\n" % tId)
+        time.sleep(1)
+    
+    print "testing lineage map"
+    lineageMap = LineageMap()
+    print "Successfully build mapping object"
+    sys.stdout.write("polar bear <-> brown bear\t")
+    brownBear = lineageMap[9644]
+    polarBear = lineageMap[29073]
+    if lineageTest(brownBear, polarBear):
+        sys.stdout.write("OK\n")
+    else:
+        sys.stdout.write("Failed. Lineages should be the same\n")
+    sys.stdout.write("E. coli <-> E. alberti\t")
+    ecoli = lineageMap[562]
+    ealbertii = lineageMap[208962]
+    if lineageTest(ecoli, ealbertii):
+        sys.stdout.write("OK\n")
+    else:
+        sys.stdout.write("Failed. Lineages be the same\n")
+    sys.stdout.write("mouse <-> rat\t")
+    mouse = lineageMap[10090]
+    rat = lineageMap[10116]
+    if lineageTest(mouse, rat):
+        sys.stdout.write("Failed. Lineages should be different\n")
+    else:
+        sys.stdout.write("OK\n")
+
+    print "done testing"
